8b48787640d4aad551c5b9ea4ca37b468aa26f18
Doughty <russell.doughty@ou.edu>
2021-09-22 16:42:04 -0500

    Updated with terra and parallel

---
+++ Aggregate_Spatial_VIs.R
@@ -0,0 +1,44 @@
+
+library(terra)
+library(parallel)
+
+fine_input    <- "/mnt/g/MCD43C4/tif/Monthly/0.05"
+coarse_output <- "/mnt/g/MCD43C4/tif/Monthly/0.25"
+temporal_res  <- "Monthly"
+vi_list       <- c("EVI", "NDVI", "NIRv", "LSWI")
+spatial_res   <- 0.25 # in degrees
+
+agg_tiff <- function (vi, in_dir, out_dir, t_res, s_res) {
+  
+  sub_dir_list <- list.dirs(paste0(in_dir, "/", vi), recursive = FALSE)
+  print(paste0("Starting ", vi, ". Input dir is ", in_dir, "/", vi))
+  
+  for (s in 1:length(sub_dir_list)) {
+    
+    temp_output_dir <- paste0(out_dir, "/", vi, "/", basename(sub_dir_list[s]))
+    print(paste0("Output dir is: ", temp_output_dir))
+    
+    if (!dir.exists(temp_output_dir)) { # Create output dirs for each year
+      dir.create(temp_output_dir, recursive = TRUE)
+    }
+    
+    sub_dir_files <- list.files(sub_dir_list[s], full.names = TRUE, pattern = "*.tif$")
+    basename(substr(sub_dir_files[s], 1, nchar(sub_dir_files[s])-4))
+    
+    for (h in 1:length(sub_dir_files)){
+      
+      out_name <- basename(substr(sub_dir_files[h], 1, nchar(sub_dir_files[h])-4)) # Get first 13 characters of filename
+      out_name <- paste0(temp_output_dir, "/", out_name, ".", s_res, ".deg.tif")
+      
+      vi_out <- rast(sub_dir_files[h])
+      vi_out <- aggregate(vi_out, fact = (s_res / 0.05), fun = mean, na.rm = TRUE)
+      
+      writeRaster(vi_out, out_name, overwrite = TRUE, NAflag = -9999 , datatype = 'INT4S')
+      
+      print(paste0("Saved file to: ", temp_output_dir, "/", out_name))
+    }
+  }
+}
+
+# Dedicate each VI instance to a core
+mclapply(vi_list, agg_tiff, mc.preschedule = TRUE, mc.cores = 4, in_dir = fine_input, out_dir = coarse_output, t_res = temporal_res, s_res = spatial_res)
