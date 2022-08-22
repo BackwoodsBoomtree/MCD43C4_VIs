cb99fc439f4caeb776ae13bc77938b7fc412f309
Russell Doughty <boomtree@schooner2.oscer.ou.edu>
2022-08-22 12:33:02 -0500

    slurm time

--- MODIS_HDF_to_TIFF_VIs.R
+++ MODIS_HDF_to_TIFF_VIs.R
@@ -220,7 +220,7 @@ sjob <- slurm_apply(save_vis, pars, vi_dir = vi_dir, qc_filter = qc_filter,
                     snow_filter = snow_filter, land_mask = land_mask,
                     jobname = 'calc_VIs', submit = TRUE, nodes = 1, cpus_per_node = 20,
                     preschedule_cores = FALSE,
-                    slurm_options = list(partition = "normal", exclusive = TRUE))
+                    slurm_options = list(partition = "normal", time = "6:00:00"))
 
 # get_job_status(sjob)[2]
 # cleanup_files(sjob)
