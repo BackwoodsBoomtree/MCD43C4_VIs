2e4226986096c376a448ba62b55df3169d339174
Russell Doughty <boomtree@schooner2.oscer.ou.edu>
2022-08-22 11:36:14 -0500

    change node cpu

--- MODIS_HDF_to_TIFF_VIs.R
+++ MODIS_HDF_to_TIFF_VIs.R
@@ -1,7 +1,7 @@
 library(terra)
 library(rslurm)
 
-terraOptions(memfrac = 0.8) # Fraction of memory to allow terra
+# terraOptions(memfrac = 0.8) # Fraction of memory to allow terra
 
 #### Input variables ####
 
@@ -218,7 +218,7 @@ save_vis     <- function(filename, vi, vi_dir, qc_filter, snow_filter, land_mask
 # Run the job
 sjob <- slurm_apply(save_vis, pars, vi_dir = vi_dir, qc_filter = qc_filter, 
                     snow_filter = snow_filter, land_mask = land_mask,
-                    jobname = 'calc_VIs', submit = TRUE, nodes = 10, cpus_per_node = 1,
+                    jobname = 'calc_VIs', submit = TRUE, nodes = 2, cpus_per_node = 20,
                     slurm_options = list(partition = "geocarb_plus"))
 
 # get_job_status(sjob)[2]
